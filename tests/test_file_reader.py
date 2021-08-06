df2ffa6763191418e82825065c9cfe73c58ad263
h-ueno2 <menbou1212@gmail.com>
2021-08-06 10:08:11 +0900

    fix: sorted test case

--- tests/test_file_reader.py
+++ tests/test_file_reader.py
@@ -1,7 +1,27 @@
-from gray_encorder.file_reader import FileReader
 import unittest
 from tempfile import TemporaryDirectory
 import os
+
+
+class IsExistsTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.tmp = TemporaryDirectory()
+
+    def tearDown(self) -> None:
+        self.tmp.cleanup()
+
+    def test_is_exsits(self):
+        from gray_encorder.file_reader import FileReader
+        test_dir = os.path.join(self.tmp.name, 'test')
+        os.mkdir(test_dir)
+        reader = FileReader(test_dir)
+        self.assertEqual(True, reader.is_exists)
+
+    def test_not_is_exists(self):
+        from gray_encorder.file_reader import FileReader
+        test_dir = os.path.join(self.tmp.name, 'test')
+        reader = FileReader(test_dir)
+        self.assertEqual(False, reader.is_exists)
 
 
 class IsImageFileTest(unittest.TestCase):
@@ -37,22 +57,3 @@ class IsImageFileTest(unittest.TestCase):
             with self.subTest(**param):
                 actual = reader.is_image_file(param['target'])
                 self.assertEqual(False, actual)
-
-
-class IsExistsTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.tmp = TemporaryDirectory()
-
-    def tearDown(self) -> None:
-        self.tmp.cleanup()
-
-    def test_is_exsits(self):
-        test_dir = os.path.join(self.tmp.name, 'test')
-        os.mkdir(test_dir)
-        reader = FileReader(test_dir)
-        self.assertEqual(True, reader.is_exists)
-
-    def test_not_is_exists(self):
-        test_dir = os.path.join(self.tmp.name, 'test')
-        reader = FileReader(test_dir)
-        self.assertEqual(False, reader.is_exists)
